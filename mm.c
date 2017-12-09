06dc019c34921094aee3cfa7db77341c83fcc7f5
kolbytn <kolbytn@gmail.com>
2017-12-09 14:31:26 -0700

    Idea for mm_free func

--- mm.c
+++ mm.c
@@ -1,6 +1,6 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
+ *
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -38,11 +38,12 @@ team_t team = {
 typedef long unsigned int mem_addr;
 
 typedef struct {
-    mem_addr next;
-    mem_addr prev;
+//    mem_addr next;
     mem_addr start;
-    unsigned int size; 
-    int alloc;
+    unsigned short prevSize; // I changed prev to size instead of pointer so the head fits into 16 bytes (We may need to test whether less space is worth the extra calculation)
+    unsigned short size; // Includes header size in size
+    char prevAlloc; // We could calculate this, but we have a few extra bytes to fill before 16 haha
+    char alloc;
 } block_hdr;
 
 mem_addr heapTop = 0;
@@ -56,21 +57,23 @@ mem_addr heapTop = 0;
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/* 
+#define HEADER_SIZE 16
+
+/*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
     block_hdr first;
     first.alloc = 0;
-    first.next = NULL;
+//    first.next = NULL;
     first.prev = NULL;
     first.start = 0;
     first.size = 500;
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
@@ -87,11 +90,19 @@ void *mm_malloc(size_t size)
 }
 
 /*
- * mm_free - Freeing a block does nothing.
+ * Checks whether the next and previous blocks are free and combines them by
+ * adjusting the size of the blocks. Then sets the alloc byte
+ *  Note: I havn't tested this yet and I'm not familiar enough w/ C to know if it will run
  */
 void mm_free(void *ptr)
 {
-
+  if(!(ptr + ptr->size)->alloc)             //if next is free
+    ptr->size += (ptr + ptr->size)->size;   //add next size to current size
+  if(!ptr->prevAlloc) {                     //if previous is free
+    ptr -= ptr->prevSize;                   //point at previous
+    ptr->size += (ptr + ptr->size)->size;   //add next size to current size
+  }
+  ptr->alloc = 0;                           //set current to free
 }
 
 /*
@@ -102,7 +113,7 @@ void *mm_realloc(void *ptr, size_t size)
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
-    
+
     newptr = mm_malloc(size);
     if (newptr == NULL)
       return NULL;
@@ -113,17 +124,3 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
     return newptr;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
