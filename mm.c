de1f7a4a4f776f0b3067a33f992615ac0328d075
kolbytn <kolbytn@gmail.com>
2017-12-09 22:27:33 -0700

    malloc best fit, not debugged

--- mm.c
+++ mm.c
@@ -38,12 +38,10 @@ team_t team = {
 typedef long unsigned int mem_addr;
 
 typedef struct {
-//    mem_addr next;
-    mem_addr start;
-    unsigned short prevSize; // I changed prev to size instead of pointer so the head fits into 16 bytes (We may need to test whether less space is worth the extra calculation)
-    unsigned short size; // Includes header size in size
-    char prevAlloc; // We could calculate this, but we have a few extra bytes to fill before 16 haha
-    char alloc;
+    unsigned short prevSize;
+    unsigned short prevAlloc;
+    unsigned short size;
+    unsigned short alloc;
 } block_hdr;
 
 mem_addr heapTop = 0;
@@ -54,10 +52,9 @@ mem_addr heapTop = 0;
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-#define HEADER_SIZE 16
+#define HEADER_SIZE 8
 
 /*
  * mm_init - initialize the malloc package.
@@ -65,29 +62,73 @@ mem_addr heapTop = 0;
 int mm_init(void)
 {
     block_hdr first;
-    first.alloc = 0;
-//    first.next = NULL;
-    first.prev = NULL;
-    first.start = 0;
+    block_hdr end;
+    first.prevSize = 0;
+    first.prevAlloc = 1;
     first.size = 500;
+    first.alloc = 0;
+    end.prevSize = 500;
+    end.prevAlloc = 0;
+    end.size = 0;
+    end.alloc = 1;
     return 0;
 }
 
 /*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
+ * mm_malloc - Look for best fit block, adn allocate more memory if does not exist
+ * Note: I havn't tested this yet and I'm not familiar enough w/ C to know if it will run
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+  block_hdr* prev;
+  block_hdr* curr = mem_heap_lo();
+  block_hdr* best = NULL;
+  unsigned short first = 0;
+
+  size = ALIGN(size);
+  while(curr->size != 0) {                         //Traverse entire heap
+    if(curr->size > size && curr->alloc == 0) {
+      if(first == 0){                              //identify first fit
+        first = curr->size;
+        best = curr;
+      }
+      else if(curr->size < best->size)             //identify best fit
+        best = curr;
     }
+    prev = curr;
+    curr += curr->size;
+  }
+
+  if(best != NULL) {
+    if(best->size - size >= 16){                    //if left over block is >= 16
+      prev = best;                                  //split block
+      curr = best + size + HEADER_SIZE;
+      curr->prevSize = prev->size;
+      curr->prevAlloc = 1;
+      curr->size = prev->size - size + HEADER_SIZE;
+      curr->alloc = 0;
+    }
+    curr = best;                                     //select best fit
+    prev = best - best->prevSize;
+  }
+  else                                               //if no best fit select end header
+    mem_sbrk(size + HEADER_SIZE);                    //and increase heap size
+
+  curr->prevSize = prev->size;                       //initialize new block
+  curr->prevAlloc = 1;
+  curr->size = size + HEADER_SIZE;
+  curr->alloc = 1;
 }
+
+  /*
+  int newsize = ALIGN(size + SIZE_T_SIZE);
+  void *p = mem_sbrk(newsize);
+  if (p == (void *)-1)
+return NULL;
+  else {
+      *(size_t *)p = size;
+      return (void *)((char *)p + SIZE_T_SIZE);
+  }*/
 
 /*
  * Checks whether the next and previous blocks are free and combines them by
@@ -96,13 +137,14 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
-  if(!(ptr + ptr->size)->alloc)             //if next is free
-    ptr->size += (ptr + ptr->size)->size;   //add next size to current size
-  if(!ptr->prevAlloc) {                     //if previous is free
-    ptr -= ptr->prevSize;                   //point at previous
-    ptr->size += (ptr + ptr->size)->size;   //add next size to current size
+  block_hdr* curr = ptr;
+  if(!(curr + curr->size)->alloc)             //if next is free
+    curr->size += (curr + curr->size)->size;   //add next size to current size
+  if(!curr->prevAlloc) {                     //if previous is free
+    curr -= curr->prevSize;                   //point at previous
+    curr->size += (curr + curr->size)->size;   //add next size to current size
   }
-  ptr->alloc = 0;                           //set current to free
+  curr->alloc = 0;                           //set current to free
 }
 
 /*
